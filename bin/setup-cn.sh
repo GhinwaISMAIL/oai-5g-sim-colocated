c5892e153e5e550ec7ea6f0bf67f70863c6c5d28
GhinwaISMAIL <ghinwa448@gmail.com>
2026-04-24 10:54:10 +0200

    fix: use oai-upf:v2.0.0 instead of oai-spgwu-tiny, fix all image tags to v2.0.0

--- bin/setup-cn.sh
+++ bin/setup-cn.sh
@@ -66,13 +66,13 @@ docker --version
 echo "[CN] Pulling OAI CN images..."
 
 docker pull mysql:8.0
-docker pull oaisoftwarealliance/oai-amf:v2.0.1
-docker pull oaisoftwarealliance/oai-smf:v2.0.1
-docker pull oaisoftwarealliance/oai-spgwu-tiny:v2.0.1
-docker pull oaisoftwarealliance/oai-nrf:v2.0.1
-docker pull oaisoftwarealliance/oai-udm:v2.0.1
-docker pull oaisoftwarealliance/oai-udr:v2.0.1
-docker pull oaisoftwarealliance/oai-ausf:v2.0.1
+docker pull oaisoftwarealliance/oai-nrf:v2.0.0
+docker pull oaisoftwarealliance/oai-amf:v2.0.0
+docker pull oaisoftwarealliance/oai-smf:v2.0.0
+docker pull oaisoftwarealliance/oai-upf:v2.0.0
+docker pull oaisoftwarealliance/oai-udm:v2.0.0
+docker pull oaisoftwarealliance/oai-udr:v2.0.0
+docker pull oaisoftwarealliance/oai-ausf:v2.0.0
 docker pull oaisoftwarealliance/trf-gen-cn5g:latest
 
 echo "[CN] All images pulled."
